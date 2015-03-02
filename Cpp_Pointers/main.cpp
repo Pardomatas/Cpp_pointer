d4273bb44bb2d758a102ab18a6a7ba814f363725
Mychal Wilson <mychal.wilson@gmail.com>
2015-03-02 14:32:14 -0500

    Testing pointers
    Teaching myself how to use pointers since my school mainly taught java.

--- Cpp_Pointers/main.cpp
+++ Cpp_Pointers/main.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int main()
 {
     int x = 5;
-    int *pointer = &x;
+    int *pointer = NULL; //uninitialized pointers should be set to Null for safety reasons.
+    *pointer = &x;
 
     cout << pointer << '\n'; //should print address
     cout << &x << '\n'; //should also print address
     cout << *pointer << '\n'; //should print 5
+
+    *pointer = 10;
+    cout << x << '\n';
     return 0;
 }
